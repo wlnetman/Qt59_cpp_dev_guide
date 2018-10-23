2c127d2669dd98c05b2229f587d35d9246254f28
wlnetman <wlnetman@gmail.com>
2018-10-23 10:37:04 +0800

    timer 1023

--- samp4_5/testtime.cpp
+++ samp4_5/testtime.cpp
@@ -1,17 +1,23 @@
+﻿#include "testtime.h"
+#include "ui_testtime.h"
 #include <QDate>
 #include <QDateTime>
 #include <QTime>
-#include "testtime.h"
-#include "ui_testtime.h"
 
-TestTime::TestTime(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::TestTime)
+TestTime::TestTime(QWidget* parent)
+    : QWidget(parent)
+    , ui(new Ui::TestTime)
 {
     ui->setupUi(this);
 
     setWindowTitle("时间和定时器");
     setWindowFlags(Qt::WindowStaysOnTopHint);
+
+    // 关联定时器
+    fTimer = new QTimer(this);
+    fTimer->stop();
+    fTimer->setInterval(1000); // 定是周期1000毫秒
+    connect(fTimer, &QTimer::timeout, this, &TestTime::on_timer_timeout);
 }
 
 TestTime::~TestTime()
@@ -19,9 +25,9 @@ TestTime::~TestTime()
     delete ui;
 }
 
-void TestTime::on_calendar_clicked(const QDate &date)
+void TestTime::on_calendar_clicked(const QDate& date)
 {
-    QString str = date.toString("yyyy-'mm-'dd");
+    QString str = date.toString("yyyy年MM月dd日");
     ui->edSelected->setText(str);
 }
 
@@ -29,7 +35,44 @@ void TestTime::on_btnTimeNow_clicked()
 {
     QDateTime local(QDateTime::currentDateTime());
     QTime t = local.time();
-    ui->spinTime->setValue(t.msec());
+    ui->spinTime->setTime(t);
     QDate d = local.date();
-    ui->cbDate->addItem(d.toString());
+    ui->dateEdit->setDate(d);
+    ui->dateTimeEdit->setDateTime(local);
+}
+
+void TestTime::on_timer_timeout()
+{
+    QTime curTime = QTime::currentTime();
+    ui->lcdHour->display(curTime.hour());
+    ui->lcdMuite->display(curTime.minute());
+    ui->lcdSecond->display(curTime.second());
+
+    int va = ui->progressBar->value();
+    va++;
+    if (va > 100)
+        va = 0;
+    ui->progressBar->setValue(va);
+}
+
+void TestTime::on_btStart_clicked()
+{
+    fTimer->start();
+    fTimeCounter.start();
+    ui->btStart->setEnabled(false);
+    ui->btStop->setEnabled(true);
+    ui->btnSetInter->setEnabled(true);
+}
+
+void TestTime::on_btStop_clicked()
+{
+    fTimer->stop();
+    int tmMsec = fTimeCounter.elapsed();
+    int ms = tmMsec % 1000;
+    int sec = tmMsec / 1000;
+    QString str = QString::asprintf("流逝时间: %d 秒， %d 毫秒", sec, ms);
+    ui->labTimer->setText(str);
+    ui->btStart->setEnabled(true);
+    ui->btStop->setEnabled(false);
+    ui->btnSetInter->setEnabled(true);
 }
--- samp4_5/testtime.h
+++ samp4_5/testtime.h
@@ -1,27 +1,34 @@
-#ifndef TESTTIME_H
+﻿#ifndef TESTTIME_H
 #define TESTTIME_H
 
+#include <QTime>
+#include <QTimer>
 #include <QWidget>
 
 namespace Ui {
 class TestTime;
 }
 
-class TestTime : public QWidget
-{
+class TestTime : public QWidget {
     Q_OBJECT
 
 public:
-    explicit TestTime(QWidget *parent = nullptr);
+    explicit TestTime(QWidget* parent = nullptr);
     ~TestTime();
 
 private slots:
-    void on_calendar_clicked(const QDate &date);
+    void on_calendar_clicked(const QDate& date);
 
     void on_btnTimeNow_clicked();
+    void on_timer_timeout(); // 定时器溢出处理
+    void on_btStart_clicked();
+
+    void on_btStop_clicked();
 
 private:
-    Ui::TestTime *ui;
+    Ui::TestTime* ui;
+    QTimer* fTimer; //定时器
+    QTime fTimeCounter; // 计时器
 };
 
 #endif // TESTTIME_H

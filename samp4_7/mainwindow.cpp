25bc9ac6bebf2385e46f657f8cdcbd2c52ca0223
wlnetman <wlnetman@gmail.com>
2018-10-26 16:34:20 +0800

    1026 listWidget menu toolbarButton

--- samp4_7/mainwindow.cpp
+++ samp4_7/mainwindow.cpp
@@ -6,11 +6,25 @@ MainWindow::MainWindow(QWidget* parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setActionForButton();
+    createSelectionPopMenu();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::setActionForButton()
+{
+    ui->tBtnListIni->setDefaultAction(ui->actListIni);
+    ui->tBtnListClear->setDefaultAction(ui->actListClear);
+    ui->tBtnListINsert->setDefaultAction(ui->actListInsert);
+    ui->tBtnListAppend->setDefaultAction(ui->actListAppend);
+    ui->tBtnListDelete->setDefaultAction(ui->actListAppend);
+    ui->tBtnSelAll->setDefaultAction(ui->actSelAll);
+    ui->tBtnSelNone->setDefaultAction(ui->actSelNone);
+    ui->tBtnSelInvs->setDefaultAction(ui->actInvs);
 }
 
 void MainWindow::on_actListIni_triggered()
@@ -25,12 +39,12 @@ void MainWindow::on_actListIni_triggered()
         aItem->setIcon(aIcon);
         aItem->setText(QString::asprintf("Item %d", i));
         aItem->setCheckState(Qt::Checked);
-        if(chk)
+        if (chk)
             aItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEditable
-                           |Qt::ItemIsUserCheckable |Qt::ItemIsEnabled);
+                | Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
         else
             aItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsUserCheckable
-                           |Qt::ItemIsEnabled);
+                | Qt::ItemIsEnabled);
     }
 }
 
@@ -47,12 +61,12 @@ void MainWindow::on_actListInsert_triggered()
     aItem->setIcon(aIcon);
     aItem->setText(QString::asprintf("Insert Item "));
     aItem->setCheckState(Qt::Checked);
-    if(ui->chkBoxListEditable->isChecked())
+    if (ui->chkBoxListEditable->isChecked())
         aItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEditable
-                       |Qt::ItemIsUserCheckable |Qt::ItemIsEnabled);
+            | Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
     else
         aItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsUserCheckable
-                       |Qt::ItemIsEnabled);
+            | Qt::ItemIsEnabled);
     int cur = ui->listWidget->currentRow();
     ui->listWidget->insertItem(cur, aItem);
     ui->listWidget->setCurrentItem(aItem);
@@ -66,12 +80,12 @@ void MainWindow::on_actListAppend_triggered()
     aItem->setIcon(aIcon);
     aItem->setText(QString::asprintf("add Item "));
     aItem->setCheckState(Qt::Checked);
-    if(ui->chkBoxListEditable->isChecked())
+    if (ui->chkBoxListEditable->isChecked())
         aItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEditable
-                       |Qt::ItemIsUserCheckable |Qt::ItemIsEnabled);
+            | Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
     else
         aItem->setFlags(Qt::ItemIsSelectable | Qt::ItemIsUserCheckable
-                       |Qt::ItemIsEnabled);
+            | Qt::ItemIsEnabled);
     ui->listWidget->addItem(aItem);
     ui->listWidget->setCurrentItem(aItem);
 }
@@ -79,6 +93,89 @@ void MainWindow::on_actListAppend_triggered()
 void MainWindow::on_actListDelete_triggered()
 {
     int cur = ui->listWidget->currentRow();
-    QListWidgetItem *item = ui->listWidget->takeItem(cur);
+    QListWidgetItem* item = ui->listWidget->takeItem(cur);
     delete item;
+}
+
+void MainWindow::on_actSelAll_triggered()
+{
+    int cnt = ui->listWidget->count();
+    for (int i = 0; i < cnt; i++) {
+        QListWidgetItem* aItem = ui->listWidget->item(i);
+        aItem->setCheckState(Qt::Checked);
+    }
+}
+
+void MainWindow::on_actSelNone_triggered()
+{
+    int cnt = ui->listWidget->count();
+    for (int i = 0; i < cnt; i++) {
+        QListWidgetItem* aItem = ui->listWidget->item(i);
+        aItem->setCheckState(Qt::Unchecked);
+    }
+}
+
+void MainWindow::on_listWidget_currentItemChanged(QListWidgetItem* current, QListWidgetItem* previous)
+{
+    QString str;
+    if (current != nullptr) {
+        if (previous == nullptr)
+            str = "当前项: " + current->text();
+        else
+            str = "前一项" + previous->text();
+        ui->editCutItemText->setText(str);
+    }
+}
+
+void MainWindow::on_actInvs_triggered()
+{
+    int cnt = ui->listWidget->count();
+    for (int i = 0; i < cnt; i++) {
+        QListWidgetItem* aItem = ui->listWidget->item(i);
+        if (aItem->checkState() == Qt::Unchecked)
+            aItem->setCheckState(Qt::Checked);
+        else
+            aItem->setCheckState(Qt::Unchecked);
+    }
+}
+
+void MainWindow::createSelectionPopMenu()
+{
+    QMenu* menuSelection = new QMenu(this);
+    menuSelection->addAction(ui->actSelNone);
+    menuSelection->addAction(ui->actSelAll);
+    menuSelection->addAction(ui->actInvs);
+
+    // tBtnSelectItem
+    ui->tBtnSelectItem->setPopupMode(QToolButton::MenuButtonPopup);
+    ui->tBtnSelectItem->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+    ui->tBtnSelectItem->setDefaultAction(ui->actSelPopMenu);
+    ui->tBtnSelectItem->setMenu(menuSelection);
+
+    //
+    QToolButton* aBtn = new QToolButton(this);
+    aBtn->setPopupMode(QToolButton::InstantPopup);
+    aBtn->setToolButtonStyle(Qt::ToolButtonTextUnderIcon);
+    aBtn->setMenu(menuSelection);
+    ui->mainToolBar->addWidget(aBtn);
+    ui->mainToolBar->addSeparator();
+    ui->mainToolBar->addAction(ui->actQuit);
+}
+
+void MainWindow::on_listWidget_customContextMenuRequested(const QPoint& pos)
+{
+    Q_UNUSED(pos);
+
+    QMenu* menuList = new QMenu(this);
+    menuList->addAction(ui->actListIni);
+    menuList->addAction(ui->actListClear);
+    menuList->addAction(ui->actListInsert);
+    menuList->addAction(ui->actListAppend);
+    menuList->addAction(ui->actListDelete);
+    menuList->addSeparator();
+    menuList->addAction(ui->actSelAll);
+    menuList->addAction(ui->actSelNone);
+    menuList->addAction(ui->actInvs);
+    menuList->exec(QCursor::pos());
+    delete menuList;
 }
--- samp4_7/mainwindow.h
+++ samp4_7/mainwindow.h
@@ -1,7 +1,11 @@
 ﻿#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QListWidget>
 #include <QMainWindow>
+#include <QMenu>
+#include <QToolBar>
+#include <QToolButton>
 
 namespace Ui {
 class MainWindow;
@@ -14,6 +18,10 @@ public:
     explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
 
+private:
+    void setActionForButton();
+    void createSelectionPopMenu();
+
 private slots:
     void on_actListIni_triggered();
 
@@ -24,6 +32,16 @@ private slots:
     void on_actListDelete_triggered();
 
     void on_actListAppend_triggered();
+
+    void on_actSelAll_triggered();
+
+    void on_listWidget_currentItemChanged(QListWidgetItem* current, QListWidgetItem* previous);
+
+    void on_actSelNone_triggered();
+
+    void on_actInvs_triggered();
+
+    void on_listWidget_customContextMenuRequested(const QPoint& pos);
 
 private:
     Ui::MainWindow* ui;
